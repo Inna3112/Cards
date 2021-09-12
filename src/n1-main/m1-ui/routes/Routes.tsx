e7250ca7cdea398495581eb673f4e6ab0db0218b
Inna3112 <innula3112@gmail.com>
2021-09-12 20:30:32 +0300

    PasswordRecover.tsx and EnterNewPassword.tsx added in pages

--- src/n1-main/m1-ui/routes/Routes.tsx
+++ src/n1-main/m1-ui/routes/Routes.tsx
@@ -3,12 +3,16 @@ import { Switch, Redirect, Route } from 'react-router-dom';
 import {Login} from './pages/Login';
 import {Profile} from './pages/Profile';
 import {Error404} from './pages/Error404';
-import {Registration} from "./pages/Registration";
+import {Registration} from './pages/Registration';
+import {PasswordRecover} from './pages/PasswordRecover';
+import {EnterNewPassword} from './pages/EnterNewPassword';
 
 export const PATH = {
     LOGIN: '/login',
     REGISTRATION: '/registration',
-    PROFILE: '/profile'
+    PROFILE: '/profile',
+    PASSWORD_RECOVER: '/password-recover',
+    ENTER_NEW_PASSWORD: '/enter-new-password',
 }
 
 export const Routes = () => {
@@ -19,6 +23,8 @@ export const Routes = () => {
             <Route path={PATH.LOGIN} render={() => <Login />}/>
             <Route path={PATH.REGISTRATION} render={() => <Registration />}/>
             <Route path={PATH.PROFILE} render={() => <Profile />}/>
+            <Route path={PATH.PASSWORD_RECOVER} render={() => <PasswordRecover />}/>
+            <Route path={PATH.ENTER_NEW_PASSWORD} render={() => <EnterNewPassword />}/>
             <Route render={() => <Error404/>}/>
 
             </Switch>
---
+++ src/n1-main/m1-ui/routes/pages/PasswordRecover.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export const PasswordRecover = () => {
+    return (
+        <div>
+            PasswordRecover
+        </div>
+    )
+}

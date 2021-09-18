f3b9017a68fcf1d739da04675c32cbbf4cb97e9b
Inna3112 <innula3112@gmail.com>
2021-09-18 12:35:29 +0300

    login added in authAPI

--- src/n1-main/m1-ui/routes/pages/Login/Login.tsx
+++ src/n1-main/m1-ui/routes/pages/Login/Login.tsx
@@ -2,14 +2,17 @@ import React, {ChangeEvent, useState} from 'react'
 import {SuperInputText} from '../../../../../common/c1-SuperInputText/SuperInputText';
 import {SuperButton} from '../../../../../common/c2-SuperButton/SuperButton';
 import s from './Login.module.css'
+import {SuperCheckbox} from '../../../../../common/c3-SuperCheckbox/SuperCheckbox';
 
 
 export const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [rememberMe, setRememberMe] = useState(false)
 
     const emailHandler = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.currentTarget.value)
     const passwordHandler = (e: ChangeEvent<HTMLInputElement>) => setPassword(e.currentTarget.value)
+    const rememberMeHandler = () => setRememberMe(!rememberMe)
     const loginHandler = () => {
         alert('email and password dispatch')
     }
@@ -19,6 +22,7 @@ export const Login = () => {
             <h1>Login</h1>
             <SuperInputText placeholder={'Email'} value={email} onChange={emailHandler}/>
             <SuperInputText placeholder={'Password'} value={password} onChange={passwordHandler}/>
+            <SuperCheckbox checked={rememberMe} onClick={rememberMeHandler}>Remember me</SuperCheckbox>
             <SuperButton onClick={loginHandler}>Login</SuperButton>
         </div>
     )
--- src/n1-main/m3-dal/api.ts
+++ src/n1-main/m3-dal/api.ts
@@ -7,7 +7,10 @@ const instance = axios.create({
 })
 
 export const authAPI = {
-register(email: string, password: string){
-    return instance.post('auth/register', {email, password})
-}
+    register(email: string, password: string) {
+        return instance.post('auth/register', {email, password})
+    },
+    login(email: string, password: string, rememberMe: boolean){
+        return instance.post('auth/login', {email, password, rememberMe})
+    }
 }

5d6107a1c1b786d0bafd01ba812db92533e72f8c
Inna3112 <innula3112@gmail.com>
2021-09-18 13:15:32 +0300

    isLoggedIn and serError added in login-reducer.ts

--- src/n1-main/m2-bll/login-reducer.ts
+++ src/n1-main/m2-bll/login-reducer.ts
@@ -1,14 +1,57 @@
-type ActionsType = any
+import {Dispatch} from 'redux';
+import {authAPI} from "../m3-dal/api";
 
-const initialLoginState = {
-
+type ActionsType = ReturnType<typeof setLogin> | ReturnType<typeof isLoggedInChange> | ReturnType<typeof setError>
+type LoginDataType = {
+    email: string,
+    password: string,
+    rememberMe: boolean,
 }
 
-export const loginReducer = (state = initialLoginState, action: ActionsType): typeof initialLoginState => {
-    switch (action.type) {
+const initialState = {
+    loginData: {
+        email: '',
+        password: '',
+        rememberMe: false,
+    },
+    isLoggedIn: false,
+    error: '',
+}
 
-        default: return state
+export const loginReducer = (state = initialState, action: ActionsType): typeof initialState => {
+    switch (action.type) {
+        case "SET-LOGIN":{
+            return {
+                ...state, loginData: {...state.loginData}
+            }
+        }
+        case "IS-LOGGED-IN-CHANGE": {
+            return {
+                ...state, isLoggedIn: action.isLoggedIn
+            }
+        }
+        case "SET-ERROR":{
+            return {
+                ...state, error: action.error
+            }
+        }
+        default:
+            return state
     }
 }
 
-export const AC = () => ({type: 'ANY-TYPE'}) as const
+export const setLogin = () => ({type: 'SET-LOGIN'}) as const
+export const isLoggedInChange = (isLoggedIn: boolean) => ({type: 'IS-LOGGED-IN-CHANGE', isLoggedIn}) as const
+export const setError = (error: string) => ({type: 'SET-ERROR', error}) as const
+
+export const loginSuccess = (loginData: LoginDataType) => (dispatch: Dispatch) => {
+    authAPI.login(loginData.email, loginData.password, loginData.rememberMe)
+        .then((res) => {
+            dispatch(setLogin())
+            dispatch(isLoggedInChange(true))
+        })
+        .catch((error) => {
+            dispatch(isLoggedInChange(false))
+            dispatch(setError(error))
+        })
+}
--- src/n1-main/m3-dal/api.ts
+++ src/n1-main/m3-dal/api.ts
@@ -5,12 +5,34 @@ const instance = axios.create({
     baseURL: 'http://localhost:7542/2.0/',
     withCredentials: true,
 })
+export type ErrorType = string
+export type LoginResponseType = {
+    avatar: string,
+    created: string,
+    deviceTokens: DeviceTokenType [],
+    email: string,
+    isAdmin: boolean,
+    name: string,
+    publicCardPacksCount: number,
+    rememberMe: boolean,
+    token: string,
+    tokenDeathTime: number,
+    updated: string,
+    verified: boolean,
+    __v: number,
+    _id: string,
+} | ErrorType
+
+type DeviceTokenType = {
+    _id: string,
+    device: string,
+}
 
 export const authAPI = {
     register(email: string, password: string) {
         return instance.post('auth/register', {email, password})
     },
-    login(email: string, password: string, rememberMe: boolean){
-        return instance.post('auth/login', {email, password, rememberMe})
+    login(email: string, password: string, rememberMe: boolean) {
+        return instance.post<LoginResponseType>('auth/login', {email, password, rememberMe})
     }
 }

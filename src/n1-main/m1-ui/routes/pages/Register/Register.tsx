686029cc721e0c96a9bf320699c687834c412273
Inna3112 <innula3112@gmail.com>
2021-09-18 11:15:30 +0300

    added TC and fixed in Register.tsx

--- src/n1-main/m1-ui/routes/pages/Register/Register.tsx
+++ src/n1-main/m1-ui/routes/pages/Register/Register.tsx
@@ -1,26 +1,28 @@
-import React, {ChangeEvent, MouseEventHandler, useState} from 'react'
+import React, {ChangeEvent, useState} from 'react'
 import {SuperInputText} from '../../../../../common/c1-SuperInputText/SuperInputText';
 import {SuperButton} from '../../../../../common/c2-SuperButton/SuperButton';
 import s from './Register.module.css'
+import {useDispatch} from 'react-redux';
+import {registerSuccess} from "../../../../m2-bll/registration-reducer";
 
 
 export const Register = () => {
+    const dispatch = useDispatch()
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirm, setConfirm] = useState('')
 
     const emailHandler = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.currentTarget.value)
-    const passwordHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        if(e.currentTarget.value === confirm){
-            setPassword(e.currentTarget.value)
-        } else {
-            //dispatch error
+    const passwordHandler = (e: ChangeEvent<HTMLInputElement>) => setPassword(e.currentTarget.value)
+    const confirmHandler = (e: ChangeEvent<HTMLInputElement>) => setConfirm(e.currentTarget.value)
+    const registerHandler = () => {
+        if(password === confirm){
+            dispatch(registerSuccess(email, password))
+        }else {
+            //error
+            alert('Password must be eaquel confirm')
         }
 
-    }
-    const confirmHandler = (e: ChangeEvent<HTMLInputElement>) => setConfirm(e.currentTarget.value)
-    const registerHandler = (e: MouseEventHandler<HTMLButtonElement>) => {
-        //dispatch
     }
 
     return (
@@ -30,7 +32,7 @@ export const Register = () => {
             <SuperInputText placeholder={'Password'} value={password} onChange={passwordHandler}/>
             <SuperInputText placeholder={'Confirm password'} value={confirm} onChange={confirmHandler}/>
             <SuperButton>Cancel</SuperButton>
-            <SuperButton>Register</SuperButton>
+            <SuperButton onClick={registerHandler}>Register</SuperButton>
         </div>
     )
 }

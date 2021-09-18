fb92c41fbaa7d912f819eccc770af825fabe15cc
Inna3112 <innula3112@gmail.com>
2021-09-18 12:00:59 +0300

    jsx added in Login.tsx

--- src/n1-main/m1-ui/routes/pages/Login.tsx
+++ src/n1-main/m1-ui/routes/pages/Login.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, {ChangeEvent, useState} from 'react'
+import {SuperInputText} from '../../../../common/c1-SuperInputText/SuperInputText';
+import {SuperButton} from '../../../../common/c2-SuperButton/SuperButton';
 
 
 export const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const emailHandler = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.currentTarget.value)
+    const passwordHandler = (e: ChangeEvent<HTMLInputElement>) => setPassword(e.currentTarget.value)
+
     return (
         <div>
-            Login
+            <h1>Login</h1>
+            <SuperInputText placeholder={'Email'} value={email} onChange={emailHandler}/>
+            <SuperInputText placeholder={'Password'} value={password} onChange={passwordHandler}/>
+            <SuperButton>Login</SuperButton>
         </div>
     )
 }

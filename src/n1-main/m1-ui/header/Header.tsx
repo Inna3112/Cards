0f42878122acb0e6cc77b950aacea9a000feb8fa
Inna3112 <innula3112@gmail.com>
2021-09-19 21:24:06 +0300

    logout button, handler, redirect added

--- src/n1-main/m1-ui/header/Header.tsx
+++ src/n1-main/m1-ui/header/Header.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import s from './Header.module.css'
-import { NavLink } from 'react-router-dom'
+import {NavLink, Redirect} from 'react-router-dom'
 import {PATH} from '../routes/Routes';
-
+import {SuperButton} from '../../../common/c2-SuperButton/SuperButton';
+import {useDispatch, useSelector} from 'react-redux';
+import {logoutSuccess} from '../../m2-bll/login-reducer';
+import {AppRootStateType} from '../../m2-bll/store';
 
 
 function Header() {
+    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.login.isLoggedIn)
+    const dispatch = useDispatch()
+
+    const logoutHandler = () => {
+        dispatch(logoutSuccess())
+    }
+    if (!isLoggedIn) {
+        return <Redirect to={'/login'}/>
+    }
     return (
         <div className={s.header}>
             <NavLink to={PATH.MAIN} className={s.link} activeClassName={s.active}>main</NavLink>
@@ -15,6 +27,7 @@ function Header() {
             <NavLink to={PATH.FORGOT} className={s.link} activeClassName={s.active}>forgot</NavLink>
             <NavLink to={PATH.SET_PASSWORD} className={s.link} activeClassName={s.active}>setPassword</NavLink>
             <NavLink to={PATH.ERROR404} className={s.link} activeClassName={s.active}>error 404</NavLink>
+            <SuperButton onClick={logoutHandler}>logout</SuperButton>
         </div>
     )
 }
